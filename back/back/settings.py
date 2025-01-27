c4864f0c4cb7f0eac6157712bc94b4885d4c90a5
Tix05 <150007929+Tix05@users.noreply.github.com>
2025-01-27 23:32:44 +0300

    creation admin et configuration postreSQL comme BDD
    ataovy jerry ny nom sy mdp an am postgreSQL

--- back/back/settings.py
+++ back/back/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gokaly',
+        'USER': 'jerry',
+        'PASSWORD': 'jerry',
+        'HOST': 'localhost',  
+        'PORT': '5432', 
     }
 }
+
 
 
 # Password validation
